4840ed5a9d69f253af17b8d60f0a25b1639f9f33
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-06 15:37:02 +0200

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Import the App component
+// src/index.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root') // This connects your React app to the root div in the HTML
-);
+  </React.StrictMode>
+)
