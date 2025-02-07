68afa921bbc6f759d76d24b2a5865b7ad8971f6e
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-07 05:56:21 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,  // Change this to whatever port you want
     proxy: {
-      '/api': 'http://localhost:3000',  // Proxy API requests to your backend
+      '/api': 'http://localhost:3000',  // Proxy API requests to the server
     },
   },
 });
