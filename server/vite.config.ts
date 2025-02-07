2ba2d160cf4449982883f823138bc0c28efaf884
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-07 09:50:52 +0200

    Update vite.config.ts

--- server/vite.config.ts
+++ server/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:1234',
+        target: 'http://localhost:1234', // Backend port
         changeOrigin: true,
       },
     },
+    port: 3000,  // React app running on port 3000
   },
 });
