671f83362e9c317a573516bdc2f1c3b617e015a2
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-07 07:55:44 +0200

    Update vite.config.ts

--- server/vite.config.ts
+++ server/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:1234', // This should match your Express server port
+        target: 'http://localhost:1234',
         changeOrigin: true,
       },
     },
-    port: 3000,  // React app should run on port 3000
   },
 });
