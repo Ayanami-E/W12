b76c028666e25a1a7215f0c9646d2645dece194b
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-07 05:06:25 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:1234',  // 代理 API 请求到 Express 后端
+      '/api': 'http://localhost:3000',  // 代理 API 请求到 Express 后端
     },
   },
 });
