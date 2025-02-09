eabec3eb62e4628d3e9382ebfe603743a59844ee
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-09 13:30:52 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import BookList from './BookList';
-import BookDetails from './BookDetails';
-import AddBook from './AddBook';
-import NotFound from './NotFound';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/book/:bookName" element={<BookDetails />} />
-        <Route path="/add" element={<AddBook />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
